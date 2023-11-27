cb20227ea9027ede06bce8de58d7b9f4d0079827
sabri <sabrinatwitter6@gmail.com>
2023-11-27 12:14:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <link rel="stylesheet" href="../app.css">
+</head>
+<body>
+    <div id="app">
+        <main>
+            <section>
+                <div class="container">
+                    <h1>Todo</h1>
+                    <input type="text">
+                </div>
+            </section>
+
+            <section>
+                <div class="container">
+                    <ul>
+                        <li>tasks</li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+    </div>
+</body>
+</html>
